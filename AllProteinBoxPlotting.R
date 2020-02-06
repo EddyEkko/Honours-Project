23e7565815c4ec1ce26027b8a99ca7d1dfb28d8c
EddyEkko <edward.agboraw@gmail.com>
2020-02-06 12:41:22 +0000

    Add files via upload

---
+++ AllProteinBoxPlotting.R
@@ -0,0 +1,5 @@
+boxplot(AllProteinCompleteCluster1$Control,AllProteinCompleteCluster1$Injury,AllProteinCompleteCluster1$Recovery, main = "All Proteins Complete Cluster1")
+
+boxplot(AllProteinCompleteCluster2$Control,AllProteinCompleteCluster2$Injury,AllProteinCompleteCluster2$Recovery, main = 'All Proteins Complete Cluster 2')
+
+#boxplot(AllProteinCompleteCluster3$Control,AllProteinCompleteCluster3$Injury,AllProteinCompleteCluster3$Recovery, main = "All Proteins Complete Cluster3")
---
+++ BoxPlotting.R
@@ -0,0 +1,5 @@
+boxplot(MOCompleteCluster1Final$Control,MOCompleteCluster1Final$Injury,MOCompleteCluster1Final$Recovery, main = "MO Complete Cluster1")
+
+boxplot(MOCompleteCluster2Final$Control,MOCompleteCluster2Final$Injury,MOCompleteCluster3Final$Recovery, main = 'MO Complete Cluster2')
+
+boxplot(MOCompleteCluster3Final$Control,MOCompleteCluster3Final$Injury,MOCompleteCluster3Final$Recovery, main = "MO COmplete Cluster3")
