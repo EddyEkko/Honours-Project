a9f32838c7bab155745549380eca1dc4fcc46720
EddyEkko <edward.agboraw@gmail.com>
2020-04-01 22:47:00 +0100

    Update proteomeVtranscriptomeConsensus.py

--- proteomeVtranscriptomeConsensus.py
+++ proteomeVtranscriptomeConsensus.py
@@ -98,3 +98,59 @@ print()
 print(len(DECluster1))
 print(len(DECluster2))
 
+#the numerators don't add up to the denomiators because, while a proteomic cluster may have 60 high quality members, not all 60 of those proteins are going to be high-value members
+#of a transcriptomic cluster.
+
+DECluster1File =open( r'C:\Users\user\Documents\pdf\bioinfo\MatrisomeOverlaps\DECluster1.txt','w')
+for x in DECluster1:
+    DECluster1File.write(x)
+    DECluster1File.write('\n')
+    
+DECluster2File =open( r'C:\Users\user\Documents\pdf\bioinfo\MatrisomeOverlaps\DECluster2.txt','w')
+for x in DECluster2:
+    DECluster2File.write(x)
+    DECluster2File.write('\n')
+
+DECluster3File =open( r'C:\Users\user\Documents\pdf\bioinfo\MatrisomeOverlaps\DECluster3.txt','w')
+for x in DECluster3:
+    DECluster3File.write(x)
+    DECluster3File.write('\n')
+    
+#gets the three transcriptomic clusters ready for GO analysis.
+#-----
+
+MOC1vsDEC1File =open( r'C:\Users\user\Documents\pdf\bioinfo\MatrisomeOverlaps\MOC1vsDEC1.txt','w')
+for x in MOC1vDEC1consensus:
+    MOC1vsDEC1File.write(x)
+    MOC1vsDEC1File.write('\n')
+    
+MOC1vsDEC2File =open( r'C:\Users\user\Documents\pdf\bioinfo\MatrisomeOverlaps\MOC1vsDEC2.txt','w')
+for x in MOC1vDEC2consensus:
+    MOC1vsDEC2File.write(x)
+    MOC1vsDEC2File.write('\n')
+    
+MOC1vsDEC3File =open( r'C:\Users\user\Documents\pdf\bioinfo\MatrisomeOverlaps\MOC1vsDEC3.txt','w')
+for x in MOC1vDEC3consensus:
+    MOC1vsDEC3File.write(x)
+    MOC1vsDEC3File.write('\n')
+
+#gets the Proteomic Cluster 1 vs Transcriptomic Clusters 1-3 overlaps ready for GO analysis
+#-----
+
+MOC2vsDEC1File =open( r'C:\Users\user\Documents\pdf\bioinfo\MatrisomeOverlaps\MOC2vsDEC1.txt','w')
+for x in MOC2vDEC1consensus:
+    MOC2vsDEC1File.write(x)
+    MOC2vsDEC1File.write('\n')
+    
+MOC2vsDEC2File =open( r'C:\Users\user\Documents\pdf\bioinfo\MatrisomeOverlaps\MOC2vsDEC2.txt','w')
+for x in MOC2vDEC2consensus:
+    MOC2vsDEC2File.write(x)
+    MOC2vsDEC2File.write('\n')
+    
+#gets the Proteomic Cluster 2 vs Transcriptomic Clusters 1-2 (since there are no P2-T3 overlaps) ready for GO analysis
+#----
+'''Follow up questions: the overlaps essentially split the proteomic clusters into different internal segments. The main question here is; do these different internal segments
+have different GO enrichments? Is MOC1vDEC1 different form MOC1vDEC2 in a biologically relevant way?'''
+    
+    
+
